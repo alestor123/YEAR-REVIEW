d6af0806370c6bbbebf09e76e53e2f0773a0fd15
alestor123 <alestoraldous@gmail.com>
2020-12-29 19:14:45 +0530

    added main code

--- App.js
+++ App.js
@@ -1,3 +1,17 @@
+
 var figlet = require('figlet'),
 axios = require('axios'),
+Box = require("cli-box"),
+name = process.argv[2] || 'alestor123'
+api = `https://user-review.glitch.me/user/${name}`
+time = new Date(),
 chalk = require('chalk');
+figlet(time.getFullYear() + ' Year Review',(err, data) => {
+if(err) throw err
+console.log(chalk.bold.green(data))
+console.log(chalk.red(time.getFullYear()+' Has been a challenging year'))
+axios.get(api).then((response)=>{
+console.log(Box("40x10", chalk.green('\n Name:'+response.data.name)+ chalk.red('\n username:'+response.data.login)+chalk.green('\n PRS+:'+response.data.pullRequests)
++ chalk.green('\n Commit:'+response.data.commits)+ chalk.green('\n Issues:'+response.data.issues)))
+})
+});
